99c3223c63824da5741f170a723a02c7e257fe64
GoncharovaDarya <63772762+GoncharovaDarya@users.noreply.github.com>
2020-09-22 18:57:32 +0300

    Add files via upload

--- Shelf.h
+++ Shelf.h
@@ -9,6 +9,8 @@
 using namespace std;
 
 class Shelf{
+
+
 	protected:
 		vector<PrintBook> pbooks;
 	public:
--- main.cpp
+++ main.cpp
@@ -6,6 +6,50 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv){
+ 
+	system ("chcp 1251");
+	
+	int mode = -1;
+	vector<BookShelf> bshelfs;
+	BookShelf bshelf;
+	while(mode!=0){
+		cout<<"-=МЕНЮ =-"<<endl;
+		cout<<"1.Добавить шкаф."<<endl;
+		cout<<"2.Добавить полку в шкаф."<<endl;
+		cout<<"3.Добавить книгу."<<endl;
+		cout<<"4.Вывод на экран."<<endl;
+		cout<<"0. Выход."<<endl;
+		cout<<"Введите номер и нажмите <ENTER>";
+		cin>>mode;
+		switch(mode){
+			case 0: continue;
+			case 1:
+				
+				bshelfs.push_back(bshelf);
+				cout<<"Шкаф добавлен!"<<endl<<endl;
+				break;
+			case 2:
+				if(bshelfs.size()>0){
+					cout<<"Доступно "<<bshelfs.size()<<" шкафов"<<endl;
+					cout<<"Введите номер шкафа ( от 1 до "<<bshelfs.size()<<"): ";
+					int n = 0;
+					cin>>n;
+					if(n>0 && n<=bshelfs.size()){
+						bshelfs[n-1].addShelf();
+						cout<<"Полка добавлена."<<endl<<endl;
+					}else{
+						cout<<"Доступные шкафы отсутствуют!"<<endl<<endl;
+						
+					}
+				}else{
+						cout<<"Доступные шкафы отсутствуют!"<<endl<<endl;
+					
+				}
+				break;
+		}
+	}
+	cout<<"Пока!";
 	return 0;
 }
+
